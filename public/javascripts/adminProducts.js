cd39e911730d518b5ed2f7195df761d08fe0baa5
EricFormar <53233372+EricM76@users.noreply.github.com>
2021-04-20 10:11:23 -0300

    agrega orden a la tabla

--- public/javascripts/adminProducts.js
+++ public/javascripts/adminProducts.js
@@ -1,6 +1,8 @@
 console.log('JS adminProducts success');
 
 const productsBox = document.getElementById('productsBox');
+const selectOrder = document.getElementById('order')
+const selectLimit = document.getElementById('limit')
 
 window.addEventListener('load', async () => {
     try {
@@ -27,13 +29,49 @@ const viewProducts = (productos) => {
             <th scope="row">${productos[i].id}</th>
             <td>${productos[i].nombre}</td>
             <td>${productos[i].descripcion}</td>
-            <td>${productos[i].precio}</td>
-            <td>${productos[i].descuento}</td>
+            <td class="text-end">$ ${productos[i].precio}</td>
+            <td class="text-center">${productos[i].descuento}%</td>
             <td>${productos[i].categoria.nombre}</td>
         </tr>
         `
         productsBox.innerHTML += item
-        
     }
 
 }
+
+selectOrder.addEventListener('change', () => {
+    const productos = JSON.parse(localStorage.getItem('productos'));
+
+    switch (selectOrder.value) {
+        case 'nombre':
+            productos.sort( (a,b) => (a.nombre > b.nombre) ? 1 : (a.nombre < b.nombre) ? -1 : 0)
+            localStorage.setItem('productos',JSON.stringify(productos));
+            viewProducts(productos)
+            break;
+        case 'mayorPrecio':
+            productos.sort( (a,b) => (a.precio < b.precio) ? 1 : (a.precio > b.precio) ? -1 : 0)
+            localStorage.setItem('productos',JSON.stringify(productos));
+            viewProducts(productos)
+            break;
+        case 'menorPrecio':
+            productos.sort( (a,b) => (a.precio > b.precio) ? 1 : (a.precio < b.precio) ? -1 : 0)
+            localStorage.setItem('productos',JSON.stringify(productos));
+            viewProducts(productos)
+            break;
+        case 'descuento':
+            productos.sort( (a,b) => (a.descuento < b.descuento) ? 1 : (a.descuento > b.descuento) ? -1 : 0)
+            localStorage.setItem('productos',JSON.stringify(productos));
+            viewProducts(productos)
+            break;
+        case 'categoria':
+            productos.sort( (a,b) => (a.categoria.nombre > b.categoria.nombre) ? 1 : (a.categoria.nombre < b.categoria.nombre) ? -1 : 0)
+            localStorage.setItem('productos',JSON.stringify(productos));
+            viewProducts(productos)
+            break;
+        default:
+            productos.sort( (a,b) => (a.id > b.id) ? 1 : (a.id < b.id) ? -1 : 0)
+            localStorage.setItem('productos',JSON.stringify(productos));
+            viewProducts(productos)
+            break;
+    }
+})
